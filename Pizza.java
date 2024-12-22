de703aaea891f90d1a24831e6f1b120e61dad1dc
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-22 21:10:18 +0300

    Update Pizza.java

--- Pizza.java
+++ Pizza.java
@@ -2,60 +2,56 @@ package pizzaBillGenerator;
 
 public class Pizza {
 
-	private int price;
-	private Boolean veg;
-	private int extraCheesePrice = 100;
-	private int extraToppingsPrice = 150;
-	private int backPackPrice = 20;
-	private int basePizzaPrice;
-	private boolean isExtraCheeseAdded = false;
-	private boolean isExtraToppingsAdded = false;
-	private boolean  isOptedForTakeAway= false;
-	
-	
-	
-	public Pizza(Boolean veg) {
+    private final int basePrice;
+    private final boolean veg;
+    private final int extraCheesePrice = 100;
+    private final int extraToppingsPrice = 150;
+    private final int takeAwayPrice = 20;
+    
+    private boolean extraCheeseAdded = false;
+    private boolean extraToppingsAdded = false;
+    private boolean takeAway = false;
 
-		this.veg = veg;
-		if(this.veg) {
-			this.price = 300;
-		}else {
-			this.price = 400;
-		}
-		basePizzaPrice = this.price;
-	}
-	public void addExtraCheese() {
-		
-		isExtraCheeseAdded = true;
-		this.price += extraCheesePrice;
-	}
-	
-	public void addExtraToppings() {
-		
-		isExtraToppingsAdded = true;
-		this.price += extraToppingsPrice;
-	}
-	
-	public void takeAway() {
-		
-		isOptedForTakeAway = true;
-		this.price += backPackPrice;
-	}
-	
-	public void getBill() {
-		
-		String bill = "";
-		System.out.println("Pizza: " + basePizzaPrice);
-		if(isExtraCheeseAdded) {
-			bill += "Extra cheese added: " + extraCheesePrice + "\n";			
-		}
-		if(isExtraToppingsAdded) {
-			bill += "Extra toppings added: " + extraToppingsPrice + "\n";
-		}
-		if (isOptedForTakeAway) {
-			bill += "Take away: " + backPackPrice + "\n";
-		}
-		bill += "Bill: " + this.price + "\n";
-		System.out.println(bill);
-	}
+    public Pizza(boolean veg) {
+        this.veg = veg;
+        this.basePrice = veg ? 300 : 400;
+    }
+
+    public void addExtraCheese() {
+        if (!extraCheeseAdded) {
+            extraCheeseAdded = true;
+        }
+    }
+
+    public void addExtraToppings() {
+        if (!extraToppingsAdded) {
+            extraToppingsAdded = true;
+        }
+    }
+
+    public void takeAway() {
+        if (!takeAway) {
+            takeAway = true;
+        }
+    }
+
+    public String getBill() {
+        int totalPrice = basePrice;
+        StringBuilder bill = new StringBuilder("Bill Breakdown:\n");
+        bill.append("Base Pizza Price: ").append(basePrice).append("\n");
+        if (extraCheeseAdded) {
+            totalPrice += extraCheesePrice;
+            bill.append("Extra Cheese: ").append(extraCheesePrice).append("\n");
+        }
+        if (extraToppingsAdded) {
+            totalPrice += extraToppingsPrice;
+            bill.append("Extra Toppings: ").append(extraToppingsPrice).append("\n");
+        }
+        if (takeAway) {
+            totalPrice += takeAwayPrice;
+            bill.append("Takeaway: ").append(takeAwayPrice).append("\n");
+        }
+        bill.append("Total Bill: ").append(totalPrice).append("\n");
+        return bill.toString();
+    }
 }
